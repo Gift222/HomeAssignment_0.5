b683753e3b6ac3236d15ef38f2123ea9f433ff58
Gift222 <giftakoto61@gmail.com>
2024-05-25 08:14:30 +0300

    Constructor

---
+++ src/Book.java
@@ -0,0 +1,22 @@
+public class Book {
+    String title;
+    String author;
+    long isbn;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+
---
+++ src/BookShop.java
@@ -0,0 +1,84 @@
+public class BookShop {
+
+
+    // Creating an instance and printing
+    public static void main(String[] args) {
+        Book book = new Book ("Things fall apart ", "Gift");
+
+        System.out.println(book.title);
+        System.out.println(book.author);
+        System.out.println(book.isbn);
+
+
+
+//Using Constructor to create multiple instances of the class with different parameter and printing the value
+        Book Book = new Book(book.title, book.author);
+        Book Book1 = new Book("Joy of motherhood", "Jason");
+        Book Book2 = new Book("Hamlet", "William");
+        Book Book3 = new Book("Oliver twist", "John");
+        Book Book4 = new Book("Romeo and Juliet", "Shakespeare");
+
+        System.out.println(Book1.title);
+        System.out.println(Book1.author);
+        System.out.println(Book1.isbn);
+
+        System.out.println(Book2.title);
+        System.out.println(Book2.author);
+        System.out.println(Book2.isbn);
+
+        System.out.println(Book3.title);
+        System.out.println(Book3.author);
+        System.out.println(Book3.isbn);
+
+        System.out.println(Book4.title);
+        System.out.println(Book4.author);
+        System.out.println(Book4.isbn);
+
+       // Task 3. Constructor with two parameters and printing them out
+        String title;
+        String author;
+
+        public Book = new Book(String title,String author) {
+        }
+            title = title;
+            author = author;
+            isbn = "Unknown";
+
+        Book book1 = new Book("Revolution", "Napoleon");
+        System.out.println(title);
+        System.out.println(author);
+
+
+        // Creating a default constructor
+        public book (String author, long isbn ) {
+
+            author = "Unknown";
+            isbn = 0000000000;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
